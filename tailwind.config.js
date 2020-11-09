f0a981a5b04f28bf8de9b6da3503da0b05a2ecaf
Daniel James <danielcolinjames@gmail.com>
2020-11-09 13:50:36 -0800

    Purge unused Tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./src/**/*.js"],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
